ccf0a21b5e3389965e12ac2f5a9229637a668301
Binaaa04 <sabrinarahmadini60@gmail.com>
2025-03-02 12:43:17 +0700

    P5_adding data in m_category using query builder

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    //
+    public function index(){
+   /* $data=[
+        'kategori_kode'=>'MKP',
+        'kategori_nama'=>'Makeup',
+        'created_at'=>now()
+    ];
+    DB::table('m_kategori')->insert($data);
+    return 'Insert a new data successful!';*/
+
+    $row = DB::table('m_kategori')->where('kategori_kode','MKP')->update(['kategori_nama'=>'Sunscreen']);
+    return 'Update data succesfull!. Total data was updated:'.$row.'baris';
+}
 }
