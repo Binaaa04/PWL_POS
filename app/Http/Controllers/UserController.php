2a3187780bfcca449f0f790dac75b52cc7cc32fb
Binaaa04 <sabrinarahmadini60@gmail.com>
2025-03-09 07:24:53 +0700

    JS4_Practicum 2_Retrieving Single Models_Section 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ use function Laravel\Prompts\password;
 class UserController extends Controller
 {
     public function index(){
-        $user = Userm::firstwhere('level_id',1)->first();
+        $user = Userm::where('level_id',1)->first();
         return view('user',['data'=>$user]);
     }
     }
