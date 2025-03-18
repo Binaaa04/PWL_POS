5ac44dde3896231326340c0a30e084aa7c232543
Binaa <sabrinarahmadini60@gmail.com>
2025-03-18 21:37:15 +0700

    modify web file

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
