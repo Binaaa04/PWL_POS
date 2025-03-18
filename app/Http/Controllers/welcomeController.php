4c043dd9c1eecd41c3b9b5cf1e801e8216a24c3e
Binaa <sabrinarahmadini60@gmail.com>
2025-03-18 14:33:29 +0700

    make welcomeController file (handle the landing page)

---
+++ app/Http/Controllers/welcomeController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title'=>'selamat datang',
+            'list'=>['home','welcome']
+        ];
+        $activeMenu = 'dashboard';
+        return view('welcome',['breadcrumb'=> $breadcrumb,'activeMenu'=> $activeMenu]);
+    }
+}
