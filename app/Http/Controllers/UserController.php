4daef2239ec33d0ec57130baf864a50453dba2cc
Binaa <sabrinarahmadini60@gmail.com>
2025-03-19 07:36:31 +0700

    create a list() function to take a list of users and return it in a format that can be used by DataTables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,48 @@
 
 namespace App\Http\Controllers;
 use App\Models\Userm;
+use App\Models\Levelm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Facades\DataTables;
 
 use function Laravel\Prompts\password;
 
 class UserController extends Controller
 {
     public function index(){
-        $user = Userm::with('level')->get(); // ubah 'm_level' jadi 'level'
-        return view('user',['data'=>$user]);
+      $breadcrumb =(object)[
+        'title'=>'User List',
+        'list'=>['Home','User']
+      ];
+      $page=(object)[
+        'title'=>'list of users registered in the system'
+      ];
+      $activeMenu = 'user'; //set menu yang sedang aktif
+      return view('user.index',['breadcrumb'=>$breadcrumb,'page'=>$page,'activeMenu'=>$activeMenu]);
     }
+    public function list(Request $request)
+    {
+        $users = Userm::select('user_id', 'username', 'nama', 'level_id')
+                ->with('level');
+
+        return DataTables::of($users)
+            ->addIndexColumn() // Menambahkan index nomor urut
+            ->addColumn('action', function ($user) { // Menambahkan kolom aksi
+                $btn  = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'" style="display:inline;">
+                            ' . csrf_field() . method_field('DELETE') . '
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Are you sure you want to delete this data?\');">Delete</button>
+                         </form>';
+                return $btn;
+            })
+            ->rawColumns(['action']) // Menandai bahwa kolom action mengandung HTML
+            ->make(true);
+    }
+
     
-    public function adding(){
+    /*public function adding(){
         return view('user_add');
     }
     public function add_save(Request $request){
@@ -43,6 +71,5 @@ class UserController extends Controller
     public function delete($id){
         $user = Userm::find($id);
         $user->delete();
-        return redirect('/user');
+        return redirect('/user');*/
     }
-}
