05243ab3544de344d08cb8bae29d0bfe132d6fe8
Binaa <sabrinarahmadini60@gmail.com>
2025-04-10 21:08:31 +0700

    add list category data with dataTables n create new category data

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -1,28 +1,80 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
 
+use App\Models\Kategorim;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    public function index(){
-   /* $data=[
-        'kategori_kode'=>'MKP',
-        'kategori_nama'=>'Makeup',
-        'created_at'=>now()
-    ];
-    DB::table('m_kategori')->insert($data);
-    return 'Insert a new data successful!';
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Categories Data ',
+            'list' => ['Home', 'Category']
+        ];
+        $page = (object)[
+            'title' => 'Categories list integreted in system'
+        ];
+        $activeMenu = 'kategori'; //set menu yang sedang aktif
 
-    $row = DB::table('m_kategori')->where('kategori_kode','MKP')->update(['kategori_nama'=>'Sunscreen']);
-    return 'Update data succesfull!. Total data was updated:'.$row.'baris';
-   
-    $row = DB::table('m_kategori')->where('kategori_kode','MKP')->delete();
-    return 'delete data succesfull!. Total data was updated:'.$row.'baris';*/
+        $kategori = Kategorim::all();
 
-    $data = DB::select('select * from m_kategori');
-    return view('kategori',['data' => $data]);
-}
+        return view('category.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'kategori' => $kategori]);
+    }
+
+    public function list(Request $request)
+    {
+        $categories = Kategorim::select('kategori_id', 'kategori_kode', 'kategori_nama');
+
+        // Filter data kategori berdasarkan kategori_id 
+        if ($request->kategori_id) {
+            $categories->where('kategori_id', $request->kategori_id);
+        }
+
+        return DataTables::of($categories)
+            ->addIndexColumn()  // menambahkan kolom index / no urut (default name kolom: DT_RowIndex)  
+            ->addColumn('action', function ($kategori) {  // menambahkan kolom action  
+                $btn  = '<a href="' . url('/kategori/' . $kategori->kategori_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/kategori/' . $kategori->kategori_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/kategori/' . $kategori->kategori_id) . '">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return 
+      confirm(\'Are u sure want to delete this data?\');">Delete</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['action']) // memberitahu bahwa kolom action adalah html  
+            ->make(true);
+    }
+    public function create()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Add New Categories',
+            'list' => ['home', 'Categories', 'Add']
+        ];
+        $page = (object)[
+            'title' => 'Add new categories'
+        ];
+        $kategori = Kategorim::all(); //ambil data kategori untuk ditampilkan di form
+        $activeMenu = 'kategori'; //set menu yang sedang aktif
+        return view('category.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'kategori' => $kategori, 'activeMenu' => $activeMenu]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kategori_kode' => 'required|string|min:3',
+            'kategori_nama' => 'required|string|max:100',
+        ]);
+
+        Kategorim::create([
+            'kategori_kode' => $request->kategori_kode,
+            'kategori_nama' => $request->kategori_nama,
+        ]);
+        return redirect('/kategori')->with('success', 'Category data succesfully changed');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,29 @@ Route::group(['prefix'=>'user'], function(){
 });
 
 Route::group(['prefix'=>'level'], function(){
-    Route::get('/', [LevelController::class, 'index']); //menampilkan halaman awal user
-    Route::post('/list', [LevelController::class, 'list']); //menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [LevelController::class, 'create']); //menampilkan halaman form tambah user
-    Route::post('/', [LevelController::class, 'store']); //menyimpan data user terbaru
-    Route::get('/{id}', [LevelController::class, 'show']); //menampilkan detail user
-    Route::get('/{id}/edit', [LevelController::class, 'edit']); //menampilkan halaman form edit user
-    Route::put('/{id}', [LevelController::class, 'update']); //menyimpan perubahan data user
-    Route::delete('/{id}', [LevelController::class, 'destroy']); //menghapus data user
+    Route::get('/', [LevelController::class, 'index']); //menampilkan halaman awal level
+    Route::post('/list', [LevelController::class, 'list']); //menampilkan data level dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']); //menampilkan halaman form tambah level
+    Route::post('/', [LevelController::class, 'store']); //menyimpan data level terbaru
+    Route::get('/{id}', [LevelController::class, 'show']); //menampilkan detail level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); //menampilkan halaman form edit level
+    Route::put('/{id}', [LevelController::class, 'update']); //menyimpan perubahan data level
+    Route::delete('/{id}', [LevelController::class, 'destroy']); //menghapus data level
+    Route::get('/create_ajax', [LevelController::class, 'create_ajax']); //menampilkan halaman form tambah level ajax
+    Route::post('/ajax', [LevelController::class, 'store_ajax']); //menyimpan data level ajax terbaru
 });
 
+Route::group(['prefix'=>'kategori'], function(){
+    Route::get('/', [KategoriController::class, 'index']); //menampilkan halaman awal kategori
+    Route::post('/list', [KategoriController::class, 'list']); //menampilkan data kategori dalam bentuk json untuk datatables
+    Route::get('/create', [KategoriController::class, 'create']); //menampilkan halaman form tambah kategori
+    Route::post('/', [KategoriController::class, 'store']); //menyimpan data kategori terbaru
+    Route::get('/{id}', [KategoriController::class, 'show']); //menampilkan detail kategori
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']); //menampilkan halaman form edit kategori
+    Route::put('/{id}', [KategoriController::class, 'update']); //menyimpan perubahan data kategori
+    Route::delete('/{id}', [KategoriController::class, 'destroy']); //menghapus data kategori
+    Route::get('/create_ajax', [KategoriController::class, 'create_ajax']); //menampilkan halaman form tambah kategori ajax
+    Route::post('/ajax', [KategoriController::class, 'store_ajax']); //menyimpan data kategori ajax terbaru
+});
 
 
