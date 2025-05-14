e07ce607f60a17073fa594edea46b323fd4fbe0b
Binaa <sabrinarahmadini60@gmail.com>
2025-05-14 15:37:46 +0700

    add import data in item

--- routes/web.php
+++ routes/web.php
@@ -98,6 +98,12 @@ Route::middleware(['auth'])->group(function () {
             Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
             Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
             Route::delete('/{id}', [BarangController::class, 'destroy']);
+            Route::get('/import', [BarangController::class, 'import']);
+            Route::post('/import_ajax',[BarangController::class, 'import_ajax']);
+            Route::get('/export_excel', [BarangController::class, 'export_excel']);
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']);
+
+        });
         });
 
         Route::prefix('penjualan')->group(function () {
@@ -143,4 +149,3 @@ Route::middleware(['auth'])->group(function () {
             });
         });
     });
-});
